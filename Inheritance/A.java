f5eb16e2f2b2727be7694c54f2af56b15a8aad13
Viraj <virajchoubey.workspace@gmail.com>
2024-04-04 20:18:14 +0530

    learning inheritance

---
+++ Inheritance/A.java
@@ -0,0 +1,10 @@
+package Inheritance;
+
+public class A {
+    static boolean checknumiseven(int x){
+        return x % 2 ==0 ? true : false;
+    }
+    A(){
+        System.out.println("In constructor A");
+    }
+}
---
+++ Inheritance/B.java
@@ -0,0 +1,19 @@
+package Inheritance;
+
+import java.util.Scanner;
+
+public class B extends A{
+
+    B(){
+        System.out.println("In constructor B");
+    }
+    public static void main(String[] args) {
+        // B obj = new B();
+        Scanner sc = new Scanner(System.in);
+        boolean ans = checknumiseven(sc.nextInt());
+        System.out.println(ans);
+        sc.close();
+        // -- now calling constructor
+        
+    }
+}
